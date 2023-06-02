e4bf35daade446b0865b7c53d45396e5bd3ebb85
ofrades <ofrades@pm.me>
2023-06-02 10:45:27 +0100

    add wezterm

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,128 +1,32 @@
 return {
-
+	-- tree
 	{
-		"mxsdev/nvim-dap-vscode-js",
-		dependencies = {
+		"nvim-neo-tree/neo-tree.nvim",
+		keys = {
+			{ "<leader>e", false },
+			{ "<leader>o", false },
 			{
-				"mfussenegger/nvim-dap",
-			},
-			{
-				"microsoft/vscode-js-debug",
-				build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-				tag = "v1.74.1",
+				"-",
+				function()
+					if vim.bo.filetype ~= "neo-tree" then
+						vim.cmd.Neotree({ "position=current", "top", "reveal" })
+					end
+				end,
+				desc = "Tree",
 			},
 		},
-		config = function()
-			local vscodeJsDebugPath = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"
-
-			require("dap-vscode-js").setup({
-				adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
-				debugger_path = vscodeJsDebugPath,
-			})
-			for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
-				require("dap").configurations[language] = {
-					{
-						type = "chrome",
-						request = "launch",
-						name = "Launch Chrome against localhost",
-						url = "http://localhost:3000",
-						webRoot = "${workspaceFolder}",
-					},
-					{
-						type = "pwa-node",
-						request = "attach",
-						name = "Attach Program (pwa-node, select pid)",
-						cwd = "${workspaceFolder}",
-						processId = require("dap.utils").pick_process,
-						skipFiles = { "<node_internals>/**" },
-					},
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Launch Current File (pwa-node)",
-						cwd = vim.fn.getcwd(),
-						args = { "${file}" },
-						sourceMaps = true,
-						protocol = "inspector",
-					},
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Launch Current File (pwa-node with ts-node)",
-						cwd = vim.fn.getcwd(),
-						runtimeArgs = { "--loader", "ts-node/esm" },
-						runtimeExecutable = "node",
-						args = { "${file}" },
-						sourceMaps = true,
-						protocol = "inspector",
-						skipFiles = { "<node_internals>/**", "node_modules/**" },
-						resolveSourceMapLocations = {
-							"${workspaceFolder}/**",
-							"!**/node_modules/**",
-						},
-					},
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Launch Current File (pwa-node with deno)",
-						cwd = vim.fn.getcwd(),
-						runtimeArgs = { "run", "--inspect-brk", "--allow-all", "${file}" },
-						runtimeExecutable = "deno",
-						attachSimplePort = 9229,
-					},
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Launch Test Current File (pwa-node with jest)",
-						cwd = vim.fn.getcwd(),
-						runtimeArgs = { "${workspaceFolder}/node_modules/.bin/jest" },
-						runtimeExecutable = "node",
-						args = { "${file}", "--coverage", "false" },
-						rootPath = "${workspaceFolder}",
-						sourceMaps = true,
-						console = "integratedTerminal",
-						internalConsoleOptions = "neverOpen",
-						skipFiles = { "<node_internals>/**", "node_modules/**" },
-					},
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Launch Test Current File (pwa-node with vitest)",
-						cwd = vim.fn.getcwd(),
-						program = "${workspaceFolder}/node_modules/vitest/vitest.mjs",
-						args = { "--inspect-brk", "--threads", "false", "run", "${file}" },
-						autoAttachChildProcesses = true,
-						smartStep = true,
-						console = "integratedTerminal",
-						skipFiles = { "<node_internals>/**", "node_modules/**" },
-					},
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Launch Test Current File (pwa-node with deno)",
-						cwd = vim.fn.getcwd(),
-						runtimeArgs = { "test", "--inspect-brk", "--allow-all", "${file}" },
-						runtimeExecutable = "deno",
-						attachSimplePort = 9229,
-					},
-					{
-						type = "pwa-chrome",
-						request = "attach",
-						name = "Attach Program (pwa-chrome, select port)",
-						program = "${file}",
-						cwd = vim.fn.getcwd(),
-						sourceMaps = true,
-						port = function()
-							return vim.fn.input("Select port: ", 9222)
-						end,
-						webRoot = "${workspaceFolder}",
-					},
-				}
-			end
-			require("dap.ext.vscode").load_launchjs(nil, {
-				["pwa-chrome"] = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
-			})
-		end,
+		opts = {
+			filesystem = {
+				hijack_netrw_behavior = "open_current",
+				-- window = {
+				-- 	mappings = {
+				-- 		["-"] = function(state)
+				-- 			require("neo-tree.ui.renderer").focus_node(state, state.tree:get_node():get_parent_id())
+				-- 		end,
+				-- 	},
+				-- },
+			},
+		},
 	},
 	-- glance
 	-- {
@@ -176,15 +80,15 @@ return {
 		},
 	},
 
-	-- theme
-	{ "ellisonleao/gruvbox.nvim", priority = 1000 },
-
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "gruvbox",
-		},
-	},
+	-- -- theme
+	-- { "ellisonleao/gruvbox.nvim", priority = 1000 },
+	--
+	-- {
+	-- 	"LazyVim/LazyVim",
+	-- 	opts = {
+	-- 		colorscheme = "gruvbox",
+	-- 	},
+	-- },
 
 	-- drop
 	{
---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,118 @@
+local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+wezterm.font_with_fallback({
+	"FantasqueSansMono Nerd Font",
+	"Ubuntu Mono",
+	"Noto Mono",
+})
+
+local scheme_for_appearance = function(appearance)
+	if appearance:find("Dark") then
+		return "tokyonight_storm"
+	else
+		return "tokyonight_day"
+	end
+end
+
+wezterm.on("window-config-reloaded", function(window)
+	local overrides = window:get_config_overrides() or {}
+	local appearance = window:get_appearance()
+	local scheme = scheme_for_appearance(appearance)
+	if overrides.color_scheme ~= scheme then
+		overrides.color_scheme = scheme
+		window:set_config_overrides(overrides)
+	end
+end)
+
+return {
+	warn_about_missing_glyphs = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "NONE",
+	check_for_updates = false,
+	leader = { mods = "CTRL", key = "a" },
+	keys = {
+		-- This will create a new split and run your default program inside it
+		{
+			key = "_",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "|",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "w",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.CloseCurrentPane({ confirm = false }),
+		},
+		{ key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
+		{ key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
+		{ key = "M", mods = "CTRL|SHIFT", action = wezterm.action.TogglePaneZoomState },
+		{
+			key = "h",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "l",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "k",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "j",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivatePaneDirection("Down"),
+		},
+		-- Show the launcher in fuzzy selection mode and have it list all workspaces
+		-- and allow activating one.
+		{
+			key = "p",
+			mods = "ALT",
+			action = wezterm.action.ShowLauncherArgs({
+				flags = "FUZZY|LAUNCH_MENU_ITEMS|WORKSPACES",
+			}),
+		},
+	},
+	font_size = 10,
+	tab_bar_at_bottom = true,
+	hide_tab_bar_if_only_one_tab = true,
+	scrollback_lines = 10000,
+	hyperlink_rules = {
+		-- Linkify things that look like URLs and the host has a TLD name.
+		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+		{
+			regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
+			format = "$0",
+		},
+
+		-- linkify email addresses
+		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+		{
+			regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+			format = "mailto:$0",
+		},
+
+		-- file:// URI
+		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+		{
+			regex = [[\bfile://\S*\b]],
+			format = "$0",
+		},
+
+		-- Linkify things that look like URLs with numeric addresses as hosts.
+		-- E.g. http://127.0.0.1:8000 for a local development server,
+		-- or http://192.168.1.1 for the web interface of many routers.
+		{
+			regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+			format = "$0",
+		},
+	},
+}
