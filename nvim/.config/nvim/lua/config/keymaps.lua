5e3dcab1b5695c31916a5d4985645196b485afa4
ofrades <ofrades@pm.me>
2024-07-22 14:36:38 +0100

    simplify

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,25 +8,19 @@ vim.keymap.set("x", "H", "^")
 vim.keymap.set("n", "L", "$")
 vim.keymap.set("o", "L", "$")
 vim.keymap.set("x", "L", "$")
-
 vim.keymap.set("n", "<C-o>", "<C-o>zz")
 vim.keymap.set("n", "<C-i>", "<C-i>zz")
-
 vim.keymap.set(
 	"n",
 	"<leader>.",
 	"<cmd>:lua require('telescope.builtin').find_files({cwd = '~/dot', hidden = true})<cr>",
 	{ desc = "Dot files" }
 )
-
 vim.keymap.set("n", "<leader>tx", "<cmd>:vsplit | term npm run test %<cr>", { desc = "Run test in split" })
 vim.keymap.set("n", "<leader>tX", "<cmd>:vsplit | term npm run test:watch %<cr>", { desc = "Run test watch in split" })
-
 vim.keymap.set("n", "qq", "<cmd>:q!<cr>", { desc = "Close" })
-
 vim.keymap.set("n", "tt", "<cmd>:vsplit | term<cr>", { desc = "Terminal" })
 vim.keymap.set("n", "tb", "<cmd>:split | term<cr>", { desc = "Terminal bottom" })
-
 vim.keymap.set("n", "<M-t>", ':exec &bg=="light" ? "set bg=dark" : "set bg=light"<CR>', { desc = "Toggle bg" })
 vim.keymap.set(
 	{ "n", "x" },
@@ -34,7 +28,6 @@ vim.keymap.set(
 	"<cmd>:lua require('telescope.builtin').grep_string()<CR>",
 	{ desc = "Toggle bg" }
 )
-
 vim.keymap.set(
 	"v",
 	"<leader>k",
--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -1,6 +1,7 @@
 return {
 	{ "echasnovski/mini.pairs", enabled = false },
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
+	{ "akinsho/bufferline.nvim", enabled = false },
 	{ "mg979/vim-visual-multi" },
 	{
 		"stevearc/oil.nvim",
