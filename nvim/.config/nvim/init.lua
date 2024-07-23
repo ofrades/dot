4d4ff93368b4d95287923e62a1a83d35f195d83d
ofrades <ofrades@pm.me>
2024-07-23 08:22:49 +0100

    life improvement

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,7 +17,6 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.test.core" },
-		{ import = "lazyvim.plugins.extras.ui.edgy" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -22,15 +22,54 @@ vim.keymap.set("n", "qq", "<cmd>:q!<cr>", { desc = "Close" })
 vim.keymap.set("n", "tt", "<cmd>:vsplit | term<cr>", { desc = "Terminal" })
 vim.keymap.set("n", "tb", "<cmd>:split | term<cr>", { desc = "Terminal bottom" })
 vim.keymap.set("n", "<M-t>", ':exec &bg=="light" ? "set bg=dark" : "set bg=light"<CR>', { desc = "Toggle bg" })
+
 vim.keymap.set(
 	{ "n", "x" },
 	"<leader>k",
 	"<cmd>:lua require('telescope.builtin').grep_string()<CR>",
-	{ desc = "Toggle bg" }
+	{ desc = "Search under cursor" }
 )
 vim.keymap.set(
 	"v",
 	"<leader>k",
-	"y<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.getreg('\"') })<CR>"
+	"y<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.getreg('\"') })<CR>",
+	{ desc = "Search selection" }
 )
-vim.keymap.set("n", "<leader>gf", "<cmd>:lua require('telescope.builtin').git_bcommits()<CR>", { desc = "Toggle bg" })
+
+vim.keymap.set(
+	"n",
+	"<leader>gf",
+	"<cmd>:lua require('telescope.builtin').git_bcommits()<CR>",
+	{ desc = "File commits" }
+)
+
+-- Function to encode URL
+local function url_encode(str)
+	return str:gsub("\n", " "):gsub("([^%w ])", function(c)
+		return string.format("%%%02X", string.byte(c))
+	end)
+end
+
+-- Function to perform the search
+local function perform_search(search_term)
+	search_term = url_encode(search_term)
+	local url = "https://www.google.com/search?q=" .. search_term
+	os.execute('open "' .. url .. '"')
+end
+
+-- Function for visual mode search
+function _G.search_visual_selection()
+	vim.cmd("normal! gvy")
+	local search_term = vim.fn.getreg('"')
+	perform_search(search_term)
+end
+
+-- Function for normal mode search (word under cursor)
+function _G.search_word_under_cursor()
+	local search_term = vim.fn.expand("<cword>")
+	perform_search(search_term)
+end
+
+-- Set up keybindings
+vim.keymap.set("v", "<leader>sg", ":<C-U>lua search_visual_selection()<CR>", { desc = "Search Google (visual)" })
+vim.keymap.set("n", "<leader>sg", ":lua search_word_under_cursor()<CR>", { desc = "Search Google (normal)" })
