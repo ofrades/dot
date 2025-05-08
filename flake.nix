901112c6364e51a80e40dd3e0bdeb9546a12f8c7
ofrades <ofrades@pm.me>
2025-05-08 23:38:32 +0100

    Revert "bring back config"
    This reverts commit a27568666dbbd7998d444a7a6c6c960362919892.

--- flake.nix
+++ flake.nix
@@ -13,11 +13,6 @@
       url = "github:Jas-SinghFSU/HyprPanel";
       inputs = { nixpkgs = { follows = "nixpkgs"; }; };
     };
-
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }:
--- modules/nvim.nix
+++ modules/nvim.nix
@@ -23,6 +23,51 @@
           { "akinsho/bufferline.nvim", enabled = false },
           { "mg979/vim-visual-multi" },
           {
+            "yetone/avante.nvim",
+            event = "VeryLazy",
+            lazy = false,
+            version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+            opts = {
+              provider = "ollama",
+              ollama = {
+                endpoint = "http://127.0.0.1:11434",
+                model = "codellama:7b",
+              },
+            },
+            build = "make",
+            dependencies = {
+              "stevearc/dressing.nvim",
+              "nvim-lua/plenary.nvim",
+              "MunifTanjim/nui.nvim",
+              {
+                -- Make sure to set this up properly if you have lazy=true
+                "MeanderingProgrammer/render-markdown.nvim",
+                opts = {
+                  file_types = { "markdown", "Avante" },
+                },
+                ft = { "markdown", "Avante" },
+              },
+            },
+          },
+          {
+            "olimorris/codecompanion.nvim",
+            dependencies = {
+              "nvim-lua/plenary.nvim",
+              "nvim-treesitter/nvim-treesitter",
+            },
+            opts = {
+              --Refer to: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
+              strategies = {
+                --NOTE: Change the adapter as required
+                chat = { adapter = "ollama" },
+                inline = { adapter = "ollama" },
+              },
+              opts = {
+                log_level = "DEBUG",
+              },
+            },
+          },
+          {
             "stevearc/oil.nvim",
             dependencies = { "nvim-tree/nvim-web-devicons" },
             opts = {
